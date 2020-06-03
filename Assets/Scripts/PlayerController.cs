407802bfb0d42c2e0aba2d6ad3a4f07858ac3d48
Matthew John Ragas <mjrag310@gmail.com>
2020-06-03 10:28:02 -0500

    Fixed character movement
    Fixed Character movement for  main ship

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,9 +14,24 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {        
-        float x = Input.mousePosition.x;
+    {
+        //Variable to hold mouse position
+        Vector3 mousePos = Input.mousePosition;
 
-        transform.position = new Vector3(x, -950.0f, 0.0f);
+        //Use the current camera to convert mouse position to a ray
+        Ray mouseRay = Camera.main.ScreenPointToRay(mousePos);
+
+        //Create a plane that faces up at the same position as the player
+        Plane playerPosPlane = new Plane(Vector3.up, transform.position);
+
+        //How far along the ray does the intersection with the plan occur?
+        float rayDistance = 0;
+        playerPosPlane.Raycast(mouseRay, out rayDistance);
+
+        //Use the ray distance to calculate the point of collision
+        Vector3 targetPoint = mouseRay.GetPoint(rayDistance);
+
+        //Move agent to target point
+        transform.position = new Vector3(targetPoint.x, -8.0f, 0.0f);
     }
 }
