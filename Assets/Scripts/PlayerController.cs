6403759f6dae568e4c8b81a635a1bdce1447b3a5
Matthew John Ragas <mjrag310@gmail.com>
2020-06-03 09:09:43 -0500

    Create PlayerController.cs
    Player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0.0f, -8.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        float x = Input.mousePosition.x;
+
+        transform.position = new Vector3(x, -950.0f, 0.0f);
+    }
+}
