30cb323eaaaa6e364e0f79d69a6817ec16a7b933
Patrick Gilfoil <s198018@students.aie.edu>
2020-06-08 17:43:04 -0500

    Roughest of Rough Drafts on Enemy AI
    A basic start.

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class EnemyBehavior : MonoBehaviour
+{
+    [SerializeField]
+    public CharacterController controller;
+    public float movementSpeed;
+    public float movementRange;
+
+    private bool positiveXDirection = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x >= movementRange || transform.position.x <= -(movementRange))
+        {
+            positiveXDirection = !positiveXDirection;
+            Debug.Log("Switch direction");
+        }
+
+        Vector3 movementDirection = new Vector3(0.0f, 0.0f, 0.0f);
+
+        switch (positiveXDirection)
+        {
+            case true:
+                movementDirection += new Vector3(movementSpeed, 0, 0);
+                break;
+            case false:
+                movementDirection += new Vector3(-movementSpeed, 0, 0);
+                break;
+        }
+
+        controller.Move(movementDirection * Time.deltaTime);
+    }
+}
