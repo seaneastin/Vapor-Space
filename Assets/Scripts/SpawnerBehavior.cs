1a60193e7c4a77dce8dbec99fa6cacfabb260dfd
Patrick Gilfoil <s198018@students.aie.edu>
2020-06-29 19:49:10 -0500

    Additional Progress
    - removed some components from merges that ended up giving conflicts
    - removed useless function in enemy behavior
    - partial additions to a reconfiguration of the enemy spawning system

--- Assets/Scripts/SpawnerBehavior.cs
+++ Assets/Scripts/SpawnerBehavior.cs
@@ -7,12 +7,15 @@ public class SpawnerBehavior : MonoBehaviour
     public int spawnScoreTotal;
     public int maxSpawnScoreTotal;
 
+    private int spawnDelay;
+    public int spawnTimer;
+
     public GameObject[] spawnables;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnDelay = spawnTimer;
     }
 
     // Update is called once per frame
@@ -20,11 +23,17 @@ public class SpawnerBehavior : MonoBehaviour
     {
         while (spawnScoreTotal < maxSpawnScoreTotal)
         {
-            foreach (GameObject g in spawnables)
+            if (spawnDelay == 0)
             {
-                Instantiate(g, new Vector3(0.0f,0.0f,0.0f), new Quaternion(0.0f,0.0f,0.0f,0.0f));
-                spawnScoreTotal++;
+                for (int g = 0; g > spawnables.Length; g++)
+                {
+                    spawnDelay = spawnTimer;
+                    Instantiate(spawnables[g], new Vector3(0.0f, 0.0f, 0.0f), new Quaternion(0.0f, 0.0f, 0.0f, 0.0f));
+                    spawnables[g].SetActive(true);
+                    spawnScoreTotal++;
+                }
             }
+            spawnDelay--;
         }
         
     }
