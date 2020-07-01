a161a2e5180b01bdf60f0f2cbba41845eefcaf92
Matthew John Ragas <mjrag310@gmail.com>
2020-07-01 18:04:16 -0500

    Added ships to scene
    Worked to add a fire rate to the player ships.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
-    public GameObject bulletPrefab;
-    public float speed = 50.0f;
-    public Rigidbody2D rb;
+{    
+
+    public float fireRate = 3.0f;
+    private float cooldown;
     
 
     public GameObject projectile;
@@ -16,7 +16,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        cooldown = fireRate;
 
         if (tag == "MainShip")
             transform.position = new Vector3(0.0f, -8.0f, 0.0f);
@@ -31,10 +31,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Deletes bullets
-        
 
-        if(tag == "MainShip")
+
+        //Firing cooldown
+        cooldown -= Time.deltaTime;
+
+        if (tag == "MainShip")
         {
             //Variable to hold mouse position
             Vector3 mousePos = Input.mousePosition;
@@ -55,10 +57,12 @@ public class PlayerController : MonoBehaviour
             //Move agent to target point
             transform.position = new Vector3(targetPoint.x, -8.0f, 0.0f);
 
-            if (Input.GetMouseButtonDown(0))
+            //Shoot bullets
+            if (Input.GetMouseButton(0) && cooldown <= 0)
             {
                 GameObject shot = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
                 shot.GetComponent<Rigidbody>().AddForce((transform.up) * shotSpeed, ForceMode.Force);
+                cooldown = fireRate;
             }
         }
         else if(tag == "RightShip")
@@ -81,6 +85,14 @@ public class PlayerController : MonoBehaviour
 
             //Move agent to target point
             transform.position = new Vector3(17.0f, targetPoint.y , 0.0f);
+
+            //Shoot bullets
+            if (Input.GetMouseButton(0) && cooldown <= 0)
+            {
+                GameObject shot = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+                shot.GetComponent<Rigidbody>().AddForce(-(transform.right) * shotSpeed, ForceMode.Force);
+                cooldown = fireRate;
+            }
         }
         else if (tag == "LeftShip")
         {
@@ -102,27 +114,14 @@ public class PlayerController : MonoBehaviour
 
             //Move agent to target point
             transform.position = new Vector3(-17.0f, targetPoint.y, 0.0f);
-        }
-        else if(tag == "TopShip")
-        {
-            //Variable to hold mouse position
-            Vector3 mousePos = Input.mousePosition;
 
-            //Use the current camera to convert mouse position to a ray
-            Ray mouseRay = Camera.main.ScreenPointToRay(mousePos);
-
-            //Create a plane that faces up at the same position as the player
-            Plane playerPosPlane = new Plane(Vector3.up, transform.position);
-
-            //How far along the ray does the intersection with the plan occur?
-            float rayDistance = 0;
-            playerPosPlane.Raycast(mouseRay, out rayDistance);
-
-            //Use the ray distance to calculate the point of collision
-            Vector3 targetPoint = mouseRay.GetPoint(rayDistance);
-
-            //Move agent to target point
-            transform.position = new Vector3(targetPoint.x, 10.0f, 0.0f);
-        }
+            //Shoot bullets
+            if (Input.GetMouseButton(0) && cooldown <= 0)
+            {
+                GameObject shot = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+                shot.GetComponent<Rigidbody>().AddForce((transform.right) * shotSpeed, ForceMode.Force);
+                cooldown = fireRate;
+            }
+        }        
     }
 }
