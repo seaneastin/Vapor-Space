b9b061cbf6d9c9970fece37c21c03e457ebc0d9b
Sean Eastin <54289093+seaneastin@users.noreply.github.com>
2020-07-06 15:14:11 -0500

    finished rubiks bomb

--- Assets/Scripts/RubiksBombBehavior.cs
+++ Assets/Scripts/RubiksBombBehavior.cs
@@ -4,25 +4,43 @@ using UnityEngine;
 
 public class RubiksBombBehavior : MonoBehaviour
 {
-    public float TimeToDestroyObject;
+    public GameObject rubiksbombsolve;
+    public GameObject rubiksbombexplode;
+    Animator rubiksbombsolveanim;
+    Animator rubiksbombexplodeanim;
+    GameObject[] enemies;
     // Start is called before the first frame update
     void Start()
     {
-        GameObject[] enemies;
+        rubiksbombsolveanim = rubiksbombsolve.GetComponent<Animator>();
+        rubiksbombexplodeanim = rubiksbombexplode.GetComponent<Animator>();
         enemies = GameObject.FindGameObjectsWithTag("Enemy");
-
-        foreach (GameObject enemy in enemies )
-        {
-            EnemyBehavior enemyBehavior;
-           enemyBehavior = enemy.GetComponent<EnemyBehavior>();
-            enemyBehavior.health = 0;
-        }
     }  
 
     // Update is called once per frame
     void Update()
     {
-        Object.Destroy(gameObject, TimeToDestroyObject);
-        
+        if (rubiksbombsolveanim.GetCurrentAnimatorStateInfo(0).IsName("Solve"))
+
+            if (rubiksbombsolveanim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+        {
+            rubiksbombsolve.SetActive(false);
+            rubiksbombexplode.SetActive(true);
+        }
+
+        if (rubiksbombexplodeanim.GetCurrentAnimatorStateInfo(0).IsName("Explode"))
+        {
+            if (rubiksbombexplodeanim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+            {
+                foreach (GameObject enemy in enemies)
+                {
+                    EnemyBehavior enemyBehavior;
+                    enemyBehavior = enemy.GetComponent<EnemyBehavior>();
+                    enemyBehavior.health = 0;
+                    
+                }
+                Object.Destroy(gameObject);
+            }
+        }
     }
 }
