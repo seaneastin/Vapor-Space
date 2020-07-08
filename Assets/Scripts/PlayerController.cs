47a46cf9570a7637ca477699ef2a94bea606cd08
Matthew John Ragas <mjrag310@gmail.com>
2020-07-08 18:22:07 -0500

    Split movement and shooting
    Created Script called CrossHairMovement.cs, splitting up the movement of the ships and the firing of the ships into seperate scripts. Ships now follow the cross hair on the screen

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,35 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{    
+{
 
-    public float fireRate = 3.0f;
     private float cooldown;
-
+   
     public GameBehavior game;
-
     public GameObject projectile;
     public float shotSpeed = 50;
+    public float fireRate = 3.0f;
 
-    public CharacterController controller;
-    public float speed;
-    
-    
+
     // Start is called before the first frame update
     void Start()
     {
         cooldown = fireRate;
 
-        if (tag == "MainShip")
-            transform.position = new Vector3(0.0f, -8.0f, 0.0f);
-        else if (tag == "RightShip")
-            transform.position = new Vector3(17.0f, 0.0f, 0.0f);
-        else if (tag == "LeftShip")
-            transform.position = new Vector3(-17.0f, 0.0f, 0.0f);
-        else if (tag == "TopShip")
-            transform.position = new Vector3(0.0f, 10.0f, 0.0f);
-
-        
+        //if (tag == "MainShip")
+        //    transform.position = new Vector3(0.0f, -8.0f, 0.0f);
+        //else if (tag == "RightShip")
+        //    transform.position = new Vector3(17.0f, 0.0f, 0.0f);
+        //else if (tag == "LeftShip")
+        //    transform.position = new Vector3(-17.0f, 0.0f, 0.0f);
     }
 
     // Update is called once per frame
@@ -45,34 +37,6 @@ public class PlayerController : MonoBehaviour
 
             if (tag == "MainShip")
             {
-                //Variable to hold mouse position
-                Vector3 mousePos = Input.mousePosition;
-
-                //Use the current camera to convert mouse position to a ray
-                Ray mouseRay = Camera.main.ScreenPointToRay(mousePos);
-
-                //Create a plane that faces up at the same position as the player
-                Plane playerPosPlane = new Plane(Vector3.up, transform.position);
-
-                //How far along the ray does the intersection with the plan occur?
-                float rayDistance = 0;
-                playerPosPlane.Raycast(mouseRay, out rayDistance);
-
-                //Use the ray distance to calculate the point of collision
-                Vector3 targetPoint = mouseRay.GetPoint(rayDistance);
-
-                //Move agent to target point
-                //transform.position = new Vector3(targetPoint.x, -8.0f, 0.0f);
-                Vector3 direction = targetPoint - transform.position;
-
-                direction.Normalize();
-
-                //direction.y = 0.0f;
-                direction.z = 0.0f;
-
-                direction *= speed;
-                controller.Move(direction * Time.deltaTime);               
-
                 //Shoot bullets
                 if (Input.GetMouseButton(0) && cooldown <= 0)
                 {
@@ -83,34 +47,6 @@ public class PlayerController : MonoBehaviour
             }
             else if (tag == "RightShip")
             {
-                //Variable to hold mouse position
-                Vector3 mousePos = Input.mousePosition;
-
-                //Use the current camera to convert mouse position to a ray
-                Ray mouseRay = Camera.main.ScreenPointToRay(mousePos);
-
-                //Create a plane that faces up at the same position as the player
-                Plane playerPosPlane = new Plane(Vector3.forward, transform.position);
-
-                //How far along the ray does the intersection with the plan occur?
-                float rayDistance = 0;
-                playerPosPlane.Raycast(mouseRay, out rayDistance);
-
-                //Use the ray distance to calculate the point of collision
-                Vector3 targetPoint = mouseRay.GetPoint(rayDistance);
-
-                //Move agent to target point
-                //transform.position = new Vector3(17.0f, targetPoint.y, 0.0f);
-                Vector3 direction = targetPoint - transform.position;
-
-                direction.Normalize();
-
-                direction.x = 0.0f;
-                direction.z = 0.0f;
-
-                direction *= speed;
-                controller.Move(direction * Time.deltaTime);
-
                 //Shoot bullets
                 if (Input.GetMouseButton(0) && cooldown <= 0)
                 {
@@ -121,34 +57,6 @@ public class PlayerController : MonoBehaviour
             }
             else if (tag == "LeftShip")
             {
-                //Variable to hold mouse position
-                Vector3 mousePos = Input.mousePosition;
-
-                //Use the current camera to convert mouse position to a ray
-                Ray mouseRay = Camera.main.ScreenPointToRay(mousePos);
-
-                //Create a plane that faces up at the same position as the player
-                Plane playerPosPlane = new Plane(Vector3.forward, transform.position);
-
-                //How far along the ray does the intersection with the plan occur?
-                float rayDistance = 0;
-                playerPosPlane.Raycast(mouseRay, out rayDistance);
-
-                //Use the ray distance to calculate the point of collision
-                Vector3 targetPoint = mouseRay.GetPoint(rayDistance);
-
-                //Move agent to target point
-                //transform.position = new Vector3(-17.0f, targetPoint.y, 0.0f);
-                Vector3 direction = targetPoint - transform.position;
-
-                direction.Normalize();
-
-                direction.x = 0.0f;
-                direction.z = 0.0f;
-
-                direction *= speed;
-                controller.Move(direction * Time.deltaTime);
-
                 //Shoot bullets
                 if (Input.GetMouseButton(0) && cooldown <= 0)
                 {
